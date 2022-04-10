a2d90a4f3c399f42d3ae92d24ba7a3aa26cca7b8
mrChocolatos <taov_rustam@mail.ru>
2022-04-10 04:04:17 +0300

    Try to found element

--- test_items.py
+++ test_items.py
@@ -1,10 +1,10 @@
 from selenium.webdriver.common.by import By
 
 
-def test_conftests(browser):
+def test_found_button(browser):
     browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
     browser.implicitly_wait(5)
     try:
-        browser.find_element(By.CSS_SELECTOR, '.btn-add-to-basket')
+        assert browser.find_element(By.CSS_SELECTOR, '.btn-add-to-basket') is not None
     finally:
         print('...Test case end.')
